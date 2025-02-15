b5126394f965380614a0c515ba05f63f841e0672
root <root@ip-172-31-7-203.ap-south-1.compute.internal>
2025-02-15 12:43:32 +0000

    Added S3 backend for Terraform state

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "terraform-state-rishav"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
   }
 }
-
